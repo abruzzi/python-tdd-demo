4709f9e5322ba438f93667a075c9c9370cc61e37
Qiu Juntao <juntao.qiu@gmail.com>
2015-10-08 18:41:16 +1100

    add exception handler

--- app/calculator.py
+++ app/calculator.py
@@ -1,3 +1,6 @@
 class Calculator:
 	def add(self, a, b):
-		return a + b
+		if isinstance(a, int) and isinstance(b, int):
+			return a + b
+		else:
+			raise ValueError
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -9,5 +9,9 @@ class CalculatorTest(unittest.TestCase):
 		ret = calc.add(1, 2)
 		self.assertEqual(ret, 3)
 
+	def test_should_throw_exception_when_no_numbers_passes(self):
+		calc = Calculator()
+		self.assertRaises(ValueError, calc.add, "1", 2)
+		
 if __name__ == "__main__":
 	unittest.main()
